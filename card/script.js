1d4e2388d8d776b0c2018596ffa135485bb5e82a
Kristina Chausheva <k.chausheva@gmail.com>
2022-02-06 02:30:24 +0200

    card menu animations

--- card/script.js
+++ card/script.js
@@ -13,35 +13,14 @@ var superToggle = function (element, class0, class1) {
 
 const moveForward = () => {
   setTimeout(() => {
+    // superToggle(arrow, "fa-arrow-left", "fa-bars")
     superToggle(bars, "fa-bars", "fa-arrow-left")
-    // bars.classList.remove("fa-bars")
-    // bars.classList.toggle("fa-arrow-left")
   }, 500)
 
   superToggle(header, "header", "header-active")
-  // header.classList.add("header-active")
-  // header.classList.remove("header")
-
   superToggle(img, "img", "img-active")
-  // img.classList.add("img-active")
-  // img.classList.remove("img")
-
   superToggle(actionContainer, "actions-container", "actions-container-active")
-  // actionContainer.classList.add("actions-container-active")
-  // actionContainer.classList.remove("actions-container")
-
   superToggle(description, "mc-description", "mc-description-active")
-  // description.classList.add("mc-description-active")
-  // description.classList.remove("mc-description")
 }
 
-// const moveBack = () => {
-//   // setTimeout(() => {}, 500)
-//   console.log("fdesfs")
-//   superToggle(arrow, "fa-arrow-left", "fa-bars")
-//   // arrow.classList.remove("fa-arrow-left")
-//   // arrow.classList.toggle("fa-bars")
-// }
-
 bars.addEventListener("click", moveForward)
-// arrow?.addEventListener("click", moveBack)
